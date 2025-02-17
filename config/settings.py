ea19ec4b2a8838792524068df9e31bd0e1bff6a8
Lochinbek Rasuljonov <lochinbekrasuljonov00@gmail.com>
2025-02-17 18:49:50 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wv!tpy1h+9w)v@5jelk=eo=aoe(5(@b78a)x#ikzfi+bwy0-jf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['exam-web.onrender.com']
+ALLOWED_HOSTS = ['https://exam-web-jdkr.onrender.com']
 
 
 # Application definition
